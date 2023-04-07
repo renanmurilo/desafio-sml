6833687eb75bc512afe8d3f7af8674f6e1e115c7
Renan Murilo dos Santos Gomes <renanmurilosantosg@gmail.com>
2023-04-07 14:19:09 -0300

    criação component pendencias

---
+++ src/components/icons/IconMetas.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <rect
+            width="48"
+            height="48"
+            rx="24"
+            fill="#FF7A00"
+            fill-opacity="0.1"
+        />
+        <path
+            d="M16 27C16 27 17 26 20 26C23 26 25 28 28 28C31 28 32 27 32 27V16C32 16 31 17 28 17C25 17 23 15 20 15C17 15 16 16 16 16M16 34L16 14"
+            stroke="#FF7A00"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+    </svg>
+</template>
--- src/pages/dashboard/Pendencias.vue
+++ src/pages/dashboard/Pendencias.vue
@@ -1,4 +1,10 @@
 <script setup>
+import EditIcon from '@/components/icons/IconEdit.vue';
+import CheckIcon from '@/components/icons/IconCheck.vue';
+import TreinoIcon from '@/components/icons/IconTreino.vue';
+import MetasIcon from '@/components/icons/IconMetas.vue';
+import IconRight from '@/components/icons/IconArrowRight.vue';
+import Dots from '@/components/icons/IconDots.vue';
 </script>
 
 <template>
@@ -6,12 +12,14 @@
         <div class="header__pendencias">
             <h3>Pendências</h3>
 
-            <a href="" class="btn__more"></a>
+            <a href="" class="btn__more"><Dots /></a>
         </div>
 
         <div class="content__pendencias">
             <div class="grid">
-                <div class="icon"></div>
+                <div class="icon">
+                    <EditIcon />
+                </div>
 
                 <div class="description">
                     <h4>Assinaturas Digitais <span>2 mins ago</span></h4>
@@ -20,40 +28,54 @@
                         validar
                     </p>
 
-                    <a href="" class="link">Resolver agora <span></span></a>
+                    <a href="" class="link">
+                        Resolver agora <span><IconRight /></span>
+                    </a>
                 </div>
             </div>
 
             <div class="grid">
-                <div class="icon"></div>
+                <div class="icon">
+                    <CheckIcon />
+                </div>
 
                 <div class="description">
                     <h4>Avaliações <span>2 mins ago</span></h4>
                     <p>Você tem <span>02 Avaliações</span> para conferir</p>
 
-                    <a href="" class="link">Resolver agora <span></span></a>
+                    <a href="" class="link">
+                        Resolver agora <span><IconRight /></span>
+                    </a>
                 </div>
             </div>
 
             <div class="grid">
-                <div class="icon"></div>
+                <div class="icon">
+                    <TreinoIcon />
+                </div>
 
                 <div class="description">
                     <h4>Treinamento <span>2 mins ago</span></h4>
                     <p>Você tem <span>18 treinamento</span> esperando você</p>
 
-                    <a href="" class="link">Resolver agora <span></span></a>
+                    <a href="" class="link">
+                        Resolver agora <span><IconRight /></span>
+                    </a>
                 </div>
             </div>
 
             <div class="grid">
-                <div class="icon"></div>
+                <div class="icon">
+                    <MetasIcon />
+                </div>
 
                 <div class="description">
                     <h4>Metas <span>2 mins ago</span></h4>
                     <p>Você tem <span>06 Metas</span> esperando você</p>
 
-                    <a href="" class="link">Resolver agora <span></span></a>
+                    <a href="" class="link">
+                        Resolver agora <span><IconRight /></span>
+                    </a>
                 </div>
             </div>
         </div>
@@ -69,5 +91,97 @@
         0px 0.0625rem 0.125rem rgba(16, 24, 40, 0.06);
     border-radius: 0.75rem;
     padding: 1.375rem 1.3125rem;
+
+    .header__pendencias {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        padding-bottom: 0.8125rem;
+        border-bottom: 0.0625rem solid $border;
+
+        h3 {
+            @include font-inter(1rem, 600, 0);
+            color: $text-light;
+        }
+    }
+
+    .content__pendencias {
+        width: 100%;
+        margin-top: 1rem;
+
+        .grid {
+            width: 100%;
+            margin-bottom: 1.75rem;
+            display: flex;
+
+            &:last-child {
+                .icon {
+                    &::before {
+                        display: none;
+                    }
+                }
+            }
+
+            .icon {
+                width: 3rem;
+                margin-right: 0.75rem;
+                position: relative;
+
+                &::before {
+                    content: '';
+                    width: 0.125rem;
+                    height: 3rem;
+                    background: $barra;
+                    position: absolute;
+                    bottom: -1.5rem;
+                    left: 0;
+                    right: 0;
+                    margin: auto;
+                }
+            }
+
+            .description {
+                margin-top: 0.3rem;
+                h4 {
+                    @include font-inter(0.875rem, 500, 0);
+                    color: $text;
+
+                    span {
+                        font-size: 0.75rem;
+                        font-weight: 400;
+                    }
+                }
+                p {
+                    @include font-inter(0.875rem, 500, 0);
+                    color: $text-light;
+                    margin: 0.5rem 0 0.75rem;
+
+                    span {
+                        font-weight: 400;
+                        color: $lilas-light;
+                    }
+                }
+
+                .link {
+                    @include font-inter(0.875rem, 600, 0);
+                    color: $blue;
+                    transition: all 0.4s;
+                    display: flex;
+
+                    span {
+                        margin-left: 0.4rem;
+                    }
+
+                    @media ($tabletMin) {
+                        &:hover {
+                            color: $lilas-dark;
+                            transition: all 0.4s;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 </style>
